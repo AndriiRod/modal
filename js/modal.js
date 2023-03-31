a97e34a0d68c030f330382b53f86673c38038909
AndriiRod <rod.andriiw@gmail.com>
2023-03-31 22:42:02 +0300

    add js

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,14 @@
 const refs = {
   openModalBtn: document.querySelector(`button[data-active="open"]`),
+  closeModalBtn: document.querySelector(`button[data-active="close"]`),
   backdrop: document.querySelector(".js-backdrop"),
+  inputName: document.querySelector(".js-form__input"),
 };
+
+function toggleModal() {
+  document.body.classList.toggle("show-modal");
+  refs.inputName.focus();
+}
+
+refs.openModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener("click", toggleModal);
