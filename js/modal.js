e33be66ba4ac0f29c057fece2cb3a55096812fe6
AndriiRod <rod.andriiw@gmail.com>
2023-04-01 17:11:48 +0300

    ready form

--- js/modal.js
+++ js/modal.js
@@ -3,17 +3,47 @@ const refs = {
   closeModalBtn: document.querySelector(`button[data-active="close"]`),
   backdrop: document.querySelector(".js-backdrop"),
   inputName: document.querySelector(".js-form__input"),
+  formSubmit: document.querySelector(".js-form-submit"),
 };
-
-function toggleModal() {
-  document.body.classList.toggle("show-modal");
-
-  if (document.body.classList.contains("show-modal"))
-    refs.backdrop.scrollTo({
-      top: document.body.scrollHeight,
-      behavior: "smooth",
-    });
-}
 
 refs.openModalBtn.addEventListener("click", toggleModal);
 refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.backdrop.addEventListener("click", toggleModalClickBackdrop);
+refs.formSubmit.addEventListener("submit", formSubmit);
+
+function smoothScroll() {
+  refs.backdrop.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: "smooth",
+  });
+}
+
+function toggleModal() {
+  document.body.classList.toggle("show-modal");
+  if (
+    window.matchMedia("(max-width: 768px)").matches &&
+    document.body.classList.contains("show-modal")
+  )
+    smoothScroll();
+  if (document.body.classList.contains("show-modal")) {
+    window.addEventListener("keydown", onEscKeyPress);
+  } else {
+    window.removeEventListener("keydown", onEscKeyPress);
+  }
+}
+
+function onEscKeyPress(event) {
+  if (event.code === "Escape") toggleModal();
+}
+
+function toggleModalClickBackdrop(event) {
+  if (event.currentTarget === event.target) toggleModal();
+}
+
+function formSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((value, name) => {
+    console.log(`${name}:${value}`);
+  });
+}
